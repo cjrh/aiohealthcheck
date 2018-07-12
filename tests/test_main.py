b15e473298942d2684d890020631df4b68c80292
Caleb Hattingh <caleb.hattingh@gmail.com>
2018-07-12 22:29:32 +1000

    Some test modifications for Windows

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,18 +8,20 @@ def test_health(loop: asyncio.AbstractEventLoop):
 
     results = []
     PORT = 5000
+    COUNT = 10
 
     async def dummy_k8s():
         # Wait for server to be up
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(0.5)
 
-        for i in range(100):
-            tup = await asyncio.open_connection(
+        for i in range(COUNT):
+            reader, writer = await asyncio.open_connection(
                 host='localhost', port=PORT, loop=loop
             )
-            reader, writer = tup
             results.append(await reader.read(1024))
             writer.close()
+            del reader
+            del writer
             print('.', end='', flush=True)
 
     t1 = loop.create_task(aiohealthcheck.tcp_health_endpoint(PORT))
@@ -28,7 +30,7 @@ def test_health(loop: asyncio.AbstractEventLoop):
     try:
         loop.run_until_complete(asyncio.wait_for(t2, 50))
     finally:
-        assert results == [b'ping'] * 100
+        assert results == [b'ping'] * COUNT
         group = asyncio.gather(t1, t2, return_exceptions=True)
         group.cancel()
         with suppress(asyncio.CancelledError):
